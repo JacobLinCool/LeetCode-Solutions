07f2ae831716d720dc4136fe27ee5071533d1c03
JacobLinCool <JacobLinCool@users.noreply.github.com>
2022-03-21 04:32:17 +0000

    Update Solutions [ci skip]

---
+++ 763. Partition Labels/partition-labels.ts
@@ -0,0 +1,22 @@
+// 763. Partition Labels (3/21/2022)
+// Runtime: 89 ms (63.64%) Memory: 44.23 MB (89.39%) 
+
+function partitionLabels(s: string): number[] {
+    const slices = [] as number[];
+    
+    let from = -1, to = -1;
+    for (let i = 0; i < s.length; i++) {
+        if (from === -1) {
+            from = i;
+        }
+        
+        to = Math.max(to, s.lastIndexOf(s[i]));
+        
+        if (i === to) {
+            slices.push(to - from + 1);
+            from = -1;
+        }
+    }
+    
+    return slices;
+};
