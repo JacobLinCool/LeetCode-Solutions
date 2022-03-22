96d09ca0ab43ddb12c47f9efb9cc9a384de09dcb
JacobLinCool <JacobLinCool@users.noreply.github.com>
2022-03-22 18:30:50 +0000

    Update Solutions [ci skip]

---
+++ 13. Roman to Integer/roman-to-integer.c
@@ -0,0 +1,26 @@
+// 13. Roman to Integer (3/23/2022)
+// Runtime: 4 ms (86.40%) Memory: 5.97 MB (22.09%) 
+
+int romanToInt (char* s) {
+    int dict[128] = { 0 };
+    dict['I'] = 1;
+    dict['V'] = 5;
+    dict['X'] = 10;
+    dict['L'] = 50;
+    dict['C'] = 100;
+    dict['D'] = 500;
+    dict['M'] = 1000;
+    
+    int len = strlen(s);
+    int ans = dict[s[len - 1]];
+    
+    for (int i = 0; i < len - 1; i++) {
+        if (dict[s[i]] >= dict[s[i + 1]]) {
+            ans += dict[s[i]];
+        } else {
+            ans -= dict[s[i]];
+        }
+    }
+    
+    return ans;
+}
--- 1663. Smallest String With A Given Numeric Value/smallest-string-with-a-given-numeric-value.ts
+++ 1663. Smallest String With A Given Numeric Value/smallest-string-with-a-given-numeric-value.ts
@@ -1,16 +1,9 @@
 // 1663. Smallest String With A Given Numeric Value (3/23/2022)
-// Runtime: 532 ms (0.00%) Memory: 69.06 MB (0.00%) 
+// Runtime: 153 ms (0.00%) Memory: 48.50 MB (66.67%) 
 
 function getSmallestString(n: number, k: number): string {
-    const ans: string[] = [];
-    
-    const a = "a".charCodeAt(0);
-
-    for (let i = n - 1; i >= 0; i--) {
-        const code = a + (k - i >= 27 ? 25 : k - i - 1);
-        ans[i] = String.fromCharCode(code);
-        k -= code - a + 1;
-    }
-
-    return ans.join("");
+    k -= n;
+    const tail = "z".repeat(Math.floor(k / 25));
+    const body = k % 25 === 0 ? "" : String.fromCharCode(k % 25 + 97);
+    return (body + tail).padStart(n, "a");
 };
