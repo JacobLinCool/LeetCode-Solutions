83ec312ffb16413d8107e8eeee78a805b5aaed02
JacobLinCool <JacobLinCool@users.noreply.github.com>
2022-03-24 10:28:50 +0000

    Update Solutions [ci skip]

---
+++ 881. Boats to Save People/boats-to-save-people.c
@@ -0,0 +1,21 @@
+// 881. Boats to Save People (3/24/2022)
+// Runtime: 112 ms (71.43%) Memory: 11.04 MB (14.29%) 
+
+int comp (int* a, int *b) {
+    return *a - *b;
+}
+
+int numRescueBoats (int people[], int people_size, int limit) {
+    qsort(people, people_size, sizeof(int), &comp);
+    
+    int heaviest = people_size - 1;
+    int lightest = 0;
+    while (lightest <= heaviest) {
+        if (people[heaviest] + people[lightest] <= limit) {
+            lightest++;
+        }
+        heaviest--;
+    }
+    
+    return people_size - heaviest - 1;
+}
