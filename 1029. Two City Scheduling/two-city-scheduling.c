10d0b84f9100a8c3b41b942630e5d55a299fa47c
JacobLinCool <JacobLinCool@users.noreply.github.com>
2022-03-25 13:12:39 +0000

    Update Solutions [ci skip]

---
+++ 1029. Two City Scheduling/two-city-scheduling.c
@@ -0,0 +1,22 @@
+// 1029. Two City Scheduling (3/25/2022)
+// Runtime: 2 ms (75.00%) Memory: 6.03 MB (50.00%) 
+
+int comp(int* a[], int* b[]) {
+    return ((*a)[0] - (*a)[1]) - ((*b)[0] - (*b)[1]);
+}
+
+int twoCitySchedCost (int* costs[], int costs_size, int* costs_col_size) {
+    qsort(costs, costs_size, sizeof(int*), &comp);
+    
+    int total = 0;
+    int half = costs_size / 2;
+    
+    for (int i = 0; i < half; i++) {
+        total += costs[i][0];
+    }
+    for (int i = half; i < costs_size; i++) {
+        total += costs[i][1];
+    }
+    
+    return total;
+}
---
+++ 1029. Two City Scheduling/two-city-scheduling.js
@@ -0,0 +1,11 @@
+// 1029. Two City Scheduling (3/25/2022)
+// Runtime: 65 ms (79.21%) Memory: 42.04 MB (70.79%) 
+
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+function twoCitySchedCost(costs) {
+    costs.sort((a, b) => (a[0] - a[1]) - (b[0] - b[1]));
+    return costs.reduce((sum, cost, i) => sum + cost[0 + (i >= costs.length / 2)], 0);
+};
