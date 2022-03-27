0a6ae151a545c4a64bb1441cbb86c5bf2980cf3c
JacobLinCool <JacobLinCool@users.noreply.github.com>
2022-03-27 06:30:06 +0000

    Update Solutions [ci skip]

---
+++ 1337. The K Weakest Rows in a Matrix/the-k-weakest-rows-in-a-matrix.js
@@ -0,0 +1,14 @@
+// 1337. The K Weakest Rows in a Matrix (3/27/2022)
+// Runtime: 100 ms (40.76%) Memory: 43.72 MB (89.17%) 
+
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+function kWeakestRows(mat, k) {
+    return mat
+        .map((_, idx) => idx)
+        .sort((a, b) => mat[a].lastIndexOf(1) - mat[b].lastIndexOf(1))
+        .slice(0, k);
+};
