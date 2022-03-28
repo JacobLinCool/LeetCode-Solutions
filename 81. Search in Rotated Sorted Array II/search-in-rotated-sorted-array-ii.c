077e83291ba00397535b3ed9a301691c9716c51e
JacobLinCool <JacobLinCool@users.noreply.github.com>
2022-03-28 08:33:27 +0000

    Update Solutions [ci skip]

---
+++ 81. Search in Rotated Sorted Array II/search-in-rotated-sorted-array-ii.c
@@ -0,0 +1,20 @@
+// 81. Search in Rotated Sorted Array II (3/28/2022)
+// Runtime: 3 ms (88.42%) Memory: 6.14 MB (17.89%) 
+
+bool search (int nums[], int nums_size, int target) {
+    bool decreased = false;
+    for (int i = 0; i < nums_size; i++) {
+        if (nums[i] == target) {
+            return true;
+        }
+        if (i < nums_size - 1) {
+            if (nums[i] > nums[i + 1]) {
+                if (decreased) {
+                    break;
+                }
+                decreased = true;
+            }
+        }
+    }
+    return false;
+}
