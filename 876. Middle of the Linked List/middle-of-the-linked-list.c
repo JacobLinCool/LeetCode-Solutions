3ab428fe8856af9af8f5f1d63021baab8323c58f
JacobLinCool <JacobLinCool@users.noreply.github.com>
2022-03-29 14:25:42 +0000

    Update Solutions [ci skip]

---
+++ 876. Middle of the Linked List/middle-of-the-linked-list.c
@@ -0,0 +1,26 @@
+// 876. Middle of the Linked List (3/29/2022)
+// Runtime: 0 ms (91.13%) Memory: 5.75 MB (75.89%) 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* middleNode (struct ListNode* head) {
+    int i = 0;
+    struct ListNode* middle = head;
+    
+    while (head) {
+        if (i % 2) {
+            middle = middle->next;
+        }
+        i++;
+        head = head->next;
+    }
+    
+    return middle;
+}
