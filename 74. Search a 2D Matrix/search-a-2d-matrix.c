3fcb90ee1dd21fdb48ca817eb59c8ae109f57aca
JacobLinCool <JacobLinCool@users.noreply.github.com>
2022-03-30 03:05:29 +0000

    Update Solutions [ci skip]

---
+++ 74. Search a 2D Matrix/search-a-2d-matrix.c
@@ -0,0 +1,19 @@
+// 74. Search a 2D Matrix (3/30/2022)
+// Runtime: 0 ms (94.10%) Memory: 6.34 MB (21.88%) 
+
+bool searchMatrix (int** matrix, int matrix_size, int* matrix_col_size, int target) {
+    int row = 0;
+    for (int i = 0; i < matrix_size; i++) {
+        if (matrix[i][0] <= target) {
+            row = i;
+        }
+    }
+    
+    for (int i = 0; i < *matrix_col_size; i++) {
+        if (matrix[row][i] == target) {
+            return true;
+        }
+    }
+    
+    return false;
+}
